5b5ae2ec9717c7be22d6b012bde42041ef9ac0ea
Jared Farkas and Sanyu Prado <sanyu@>
2019-01-24 09:32:09 -0800

    add rough draft

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+var vowels = ["a","A","e","E","i","I","o","O","u","U"];
+function pigLatin(input){
+  if(vowels.includes(input[0])){
+    input = input.join('') + "way";
+  }else{
+    if (input.length > 1){
+      var i = 0;
+      while(vowels.includes(input[i])===false)
+      {
+          i++;
+      }
+      if(input[i-1] === 'q' || input[i-1] === 'Q')
+      {
+        i++;
+      }
+      input = input.slice(i,input.length).concat(input.slice(0,i)).join('')+"ay";
+    }else{
+      input = input.join('');
+    }
+  }
+  return input;
+}
+
+$(document).ready(function(){
+  $("form#transportation_survey").submit(function(event){
+      var input = $("#phrase").val().split('');
+      var input = pigLatin(input);
+    console.log(input);
+    event.preventDefault();
+    });
+  });
